ced6aa7a6225eabc5dc8eabd1eaf51f9fe1b8bd1
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-12-05 16:09:57 -0500

    basic contact form structure

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,9 +2,31 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>
-            Contact
-        </div>
+        <div className="row">
+        <h3 className='header'>Contact <span><img src='/favicon.ico' alt='aboutIcon'></img></span></h3>
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <i className="material-icons prefix">account_circle</i>
+              <input id="first_name" type="text" className="validate"/>
+              <label for="first_name">Name</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+              <input id="email" type="email" className="validate"/>
+              <label for="email">Email</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s12">
+            <textarea id="textarea1" className="materialize-textarea"></textarea>
+            <label for="textarea1">Textarea</label>
+            </div>
+          </div>
+          
+        </form>
+      </div>
     )
 }
 
