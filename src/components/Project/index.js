5cb752f88955cec4d2740434ce895419663ae837
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-12-05 15:58:37 -0500

    typo change

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,7 +5,7 @@ const Project = (props) => {
         <div className='col s12 m4 l4'>
         <div className="card hoverable">
             <div className="card-image waves-effect waves-block waves-light">
-                <img className="activator" alt={props.name} src={props.image}/>
+                <img className="activator responsive-img" alt={props.name} src={props.image}/>
             </div>
             <div className="card-content">
                 <span className="card-title activator grey-text text-darken-4">{props.name}<i className="material-icons right">more_vert</i></span>
