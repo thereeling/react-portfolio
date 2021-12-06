602e06094dc391cb7f733578c4f3dd7e4e95f332
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-12-05 20:39:47 -0500

    styled cards in portfolio

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,22 +1,23 @@
 import React from "react";
+import StyledIcon from "../StylingComponents/StyledIcon";
 
 const Project = (props) => {
     return (
         <div className='col s12 m4 l4'>
-        <div className="card hoverable">
+        <div className="card hoverable grey darken-3">
             <div className="card-image waves-effect waves-block waves-light">
                 <img className="activator responsive-img" alt={props.name} src={props.image}/>
             </div>
             <div className="card-content">
-                <span className="card-title activator grey-text text-darken-4">{props.name}<i className="material-icons right">more_vert</i></span>
+                <span className="card-title activator">{props.name}<i className="material-icons right">more_vert</i></span>
                 <p>
-                    <a href={props.github} target='_blank' rel="noreferrer"><i className="fab fa-github"></i></a>
-                    <a href={props.deploy} target='_blank' rel="noreferrer"><i className="fas fa-rocket"></i></a>
+                    <a href={props.github} target='_blank' rel="noreferrer"><StyledIcon className="waves-effect waves-red small fab fa-github"></StyledIcon></a>
+                    <a href={props.deploy} target='_blank' rel="noreferrer"><StyledIcon className="waves-effect waves-red small fas fa-rocket"></StyledIcon></a>
                 </p>
             </div>
             <div className="card-reveal">
-                <span className="card-title grey-text text-darken-4">{props.name}<i className="material-icons right">close</i></span>
-                <p>{props.description}</p>
+                <span className="card-title grey-text text-darken-3">{props.name}<i className="material-icons right">close</i></span>
+                <p className='grey-text text-darken-3'>{props.description}</p>
             </div>
         </div>
         </div>
---
+++ src/components/StylingComponents/StyledIcon.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledIcon = styled.i`
+&&&{
+    color: var(--turquoise);
+    margin: 0.5em;
+}
+`
+
+export default StyledIcon
