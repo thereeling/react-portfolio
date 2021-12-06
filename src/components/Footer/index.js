699d45c200da045d86b9cb70477ab9f1bba0508b
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-12-06 01:13:22 -0500

    footer styling complete

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import StyledFooterIcon from '../StylingComponents/StyledFooterIcon';
 
 const Footer = () => {
 
@@ -6,14 +7,14 @@ const Footer = () => {
         <footer style={{backgroundColor: 'var(--dark-gray)'}}className="page-footer">
           <div className="container">
             <div className="row">
-              <div className="col l6 s12">
-                <h5 className="white-text">Footer Content</h5>
-                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              <div className="col s12 center">
+              <a href='https://github.com/thereeling' target='_blank' rel="noreferrer"><StyledFooterIcon className="waves-effect waves-red small fab fa-github"></StyledFooterIcon></a>
+              <a href='https://www.linkedin.com/in/ngiuliani94/' target='_blank' rel="noreferrer"><StyledFooterIcon className="waves-effect waves-red small fab fa-linkedin"></StyledFooterIcon></a>
               </div>
             </div>
           </div>
           <div className="footer-copyright">
-            <div className="container">
+            <div className="container center">
             © 2014 Nick Giuliani
             </div>
           </div>
---
+++ src/components/StylingComponents/StyledFooterIcon.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import StyledIcon from "./StyledIcon";
+
+const StyledFooterIcon = styled(StyledIcon)`
+&&&{
+    color: var(--red);
+    margin: 0.5em;
+}
+`
+
+export default StyledFooterIcon
