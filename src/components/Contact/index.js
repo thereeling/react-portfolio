0e858640f28e0ed55dc2470e346e6af5a40928c5
Nick Giuliani <nicholas.giuliani94@gmail.com>
2021-12-05 19:15:20 -0500

    removed some css

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,23 +6,30 @@ const Contact = () => {
         <h3 className='header'>Contact <span><img src='/favicon.ico' alt='aboutIcon'></img></span></h3>
         <form className="col s12">
           <div className="row">
-            <div className="input-field col s6">
+            <div className="input-field col s12 m6 l6">
               <i className="material-icons prefix">account_circle</i>
               <input id="first_name" type="text" className="validate"/>
               <label for="first_name">Name</label>
             </div>
           </div>
           <div className="row">
-            <div className="input-field col s12">
+            <div className="input-field col s12 m6 l6">
+              <i className="material-icons prefix">email</i>
               <input id="email" type="email" className="validate"/>
               <label for="email">Email</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s12">
+            <i className="material-icons prefix">edit</i>
             <textarea id="textarea1" className="materialize-textarea"></textarea>
             <label for="textarea1">Textarea</label>
             </div>
+          </div>
+          <div className='row'>
+            <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+            <i className="material-icons right">send</i>
+            </button>
           </div>
           
         </form>
