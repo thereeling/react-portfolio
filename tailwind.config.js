f9fc7ce5b3d5e01e912307f653828ad14038b5c6
Nick <nicholas.giuliani94@gmail.com>
2024-07-14 13:37:27 -0400

    Initial commit from Create Next App

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+<<<<<<< HEAD
 export default {
   content: ["index.html", "./src/**/*.{jsx,js}"],
   darkMode: "class", 
@@ -7,9 +8,27 @@ export default {
       fontFamily: {
         heading: ['var(--font-heading)', ...fontFamily.mono],
         body: ['var(--font-body)', ...fontFamily.mono]
+=======
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+>>>>>>> bdec42c (Initial commit from Create Next App)
       },
     },
   },
   plugins: [],
+<<<<<<< HEAD
 }
 
+=======
+};
+>>>>>>> bdec42c (Initial commit from Create Next App)
